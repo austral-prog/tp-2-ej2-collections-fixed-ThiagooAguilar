23000cbaef9b7288e1d0f067f8283a7336f6d0e5
Thiago Aguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2024-08-22 00:22:03 -0300

    Update Sets.java

--- src/main/java/com/collections/Sets.java
+++ src/main/java/com/collections/Sets.java
@@ -10,4 +10,14 @@ public class Sets {
             "tequila", "gin", "dry vermouth", "sweet vermouth", "prosecco","aperol", "brandy", "mezcal",
             "triple sec", "coffee liqueur", "almond liqueur", "champagne", "orange curacao", "rum"));
 
+    //mplementa la función check_drinks que tome el nombre de una bebida y una lista de ingredientes. La función debería devolver el nombre de la bebida seguido de "Mocktail" si la bebida no tiene ingredientes alcohólicos, y el nombre de la bebida seguido de "Cocktail" si la bebida incluye alcohol. Para los propósitos de este ejercicio, los cócteles solo incluirán alcoholes del set ALCOHOLS en sets_categories_data.py:
+    public String checkDrinks(String drink, List<String> ingredients) {
+        for (int i = 0; i < ingredients.size(); i++) {
+            if (ALCOHOLS.contains(ingredients.get(i))){
+                return String.format("%s Cocktail", drink);
+            }
+        }
+        return String.format("%s Mocktail", drink);
+    }
+
 }
